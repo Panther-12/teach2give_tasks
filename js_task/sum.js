d205af691b4592e6148d434314254d6e02dea5d4
Panther-12 <nimrodwalwe@gmail.com>
2024-05-15 09:52:50 +0300

    multiple solutions

--- js_task/sum.js
+++ js_task/sum.js
@@ -9,3 +9,11 @@ const array1 = [
 const sum4 = array1.map(item => item.price).reduce((accumulator, price) => accumulator + price, 0)
 console.log(`The sum of ${array1[0].price} + ${array1[0].price} + ${array1[0].price} = ${sum4}`) // 46.97
 
+
+var sum = 0
+const sum5 = array1.map(item => item.price)
+console.log(sum5[0]+sum5[1]+sum5[2])
+
+var sum = 0
+const sum6 = array1[0].price + array1[1].price +array1[2].price
+console.log(sum6)
